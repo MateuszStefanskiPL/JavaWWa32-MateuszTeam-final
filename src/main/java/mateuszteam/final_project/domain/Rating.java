ba5fcabb1a88ffb0724e2bec959ecb4231acbe2c
84MateuszStefanski <84mateuszstefanski@gmail.com>
2022-02-20 21:17:12 +0100

    Rating entity fields created ask for a code review

--- src/main/java/mateuszteam/final_project/domain/Rating.java
+++ src/main/java/mateuszteam/final_project/domain/Rating.java
@@ -1,6 +1,35 @@
 package mateuszteam.final_project.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rating")
 public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "rating_id")
+    public long ratingId;
+
+    @NotNull
+    @Column(name = "user_id")
+    private long userId;
+
+    @NotNull
+    @Column(name = "movie_id")
+    private long movieId;
+
+    @NotNull
+    @Column(name = "rating")
+    private double ratingScore;
 
 
 }
