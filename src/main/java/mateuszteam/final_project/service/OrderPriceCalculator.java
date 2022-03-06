3723eb8014c48405fc8682c85c7f019baa25931f
84MateuszStefanski <84mateuszstefanski@gmail.com>
2022-03-06 13:56:55 +0100

    working on price calculator order price is now calculated without time variable

--- src/main/java/mateuszteam/final_project/service/OrderPriceCalculator.java
+++ src/main/java/mateuszteam/final_project/service/OrderPriceCalculator.java
@@ -2,6 +2,7 @@ package mateuszteam.final_project.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mateuszteam.final_project.domain.entities.MovieStatus;
 import mateuszteam.final_project.domain.entities.MoviesOrder;
 import mateuszteam.final_project.domain.entities.UserStatus;
 import mateuszteam.final_project.repository.MoviesCopiesRepository;
@@ -21,50 +22,61 @@ public class OrderPriceCalculator {
     private static final BigDecimal SILVER_USER_DISCOUNT = BigDecimal.valueOf(0.95D);
     private static final BigDecimal GOLD_USER_DISCOUNT = BigDecimal.valueOf(0.85D);
     private static final BigDecimal PLATINUM_USER_DISCOUNT = BigDecimal.valueOf(0.70D);
+
     private static final BigDecimal PREMIERE_MOVIE_PRICE_IMPACT = BigDecimal.valueOf(3.0D);
     private static final BigDecimal NEWEST_MOVIE_PRICE_IMPACT = BigDecimal.valueOf(2.0D);
     private static final BigDecimal CLASSIC_MOVIE_PRICE_IMPACT = BigDecimal.valueOf(0.5D);
 
 
-
-
-
     private final OrdersRepository ordersRepository;
     private final MoviesCopiesRepository copiesRepository;
 
-    public BigDecimal calculateOrderPriceByUserStatus(UserStatus userStatus){
-        var finalPrice = STANDARD_MOVIE_PRICE;
-        switch (userStatus){
-            case NEW_USER:
-                finalPrice = STANDARD_MOVIE_PRICE;
-                break;
-            case SILVER:
-               // finalPrice = STANDARD_MOVIE_PRICE.multiply();
-        }
-        return null;
-    }
 
     public BigDecimal calculateOrderPrice(MoviesOrder order){
         var userStatus = order.getUser().getUserStatus();
         var movieStatus = order.getMovieCopies().stream().findFirst().get().getMovie().getMovieStatus();
-       // var finalPrice = BigDecimal.valueOf(STANDARD_MOVIE_PRICE);
+        var totalPrice = calculateOrderPriceByMovieStatus(movieStatus,userStatus);
 
-
-
-        return null;
+        return totalPrice;
     }
 
-    //client status
-    //movie status
-    //order time
-//todo jak zorganizować obliczanie czasu oddania skoro
-// tylko zmieniamy status i nie ma jak pobrać daty oddania jedynie może eventem przy kazdej
-// zmianie statusu aktualizować cene
+    private BigDecimal calculateOrderPriceByUserStatus(UserStatus userStatus){
+        var finalPrice = STANDARD_MOVIE_PRICE;
+        switch (userStatus){
+            case SILVER:
+                finalPrice = STANDARD_MOVIE_PRICE.multiply(SILVER_USER_DISCOUNT);
+                break;
+            case GOLD:
+                finalPrice = STANDARD_MOVIE_PRICE.multiply(GOLD_USER_DISCOUNT);
+                break;
+            case PLATINUM:
+                finalPrice = STANDARD_MOVIE_PRICE.multiply(PLATINUM_USER_DISCOUNT);
+                break;
+            default:
+                finalPrice = STANDARD_MOVIE_PRICE;
+        }
+        return finalPrice;
+    }
 
+    private BigDecimal calculateOrderPriceByMovieStatus(MovieStatus movieStatus,UserStatus userStatus){
+        var finalPrice = calculateOrderPriceByUserStatus(userStatus);
 
-//calcculate prica by user status
-    // calculate price by movie status
-
+        switch (movieStatus){
+            case CLASSIC:
+                finalPrice.multiply(CLASSIC_MOVIE_PRICE_IMPACT);
+                break;
+            case NEWEST:
+                finalPrice.multiply(NEWEST_MOVIE_PRICE_IMPACT);
+                break;
+            case PREMIERE:
+                finalPrice.multiply(PREMIERE_MOVIE_PRICE_IMPACT);
+                break;
+        }
+        return finalPrice;
+    }
 
 
 }
+//todo jak zorganizować obliczanie czasu oddania skoro
+// tylko zmieniamy status i nie ma jak pobrać daty oddania jedynie może eventem przy kazdej
+// zmianie statusu aktualizować cene
